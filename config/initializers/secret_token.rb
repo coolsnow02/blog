222c5b6f6126834c9c8b9f6c6490c30beade5826
nehagwebonise <webonise@webonise-desktop.(none)>
2013-01-15 11:47:31 +0530

    adding existing stable code to develop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b442a00dfd51f32ef4f19235a384656d2baa0ace1464f3a3c37ed731705d86cc2f919b09f494a68bccc7743826c8ece510df4a396165860e6e848d68a371b309'
